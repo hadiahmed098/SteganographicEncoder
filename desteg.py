98271121ad7abd2734ca38b29f390419bff2e76a
Hadi Ahmed <hadiahmed098@gmail.com>
2020-02-15 23:52:58 -0500

    Decoding properly completed, program will read all LSB in an image and return a text file with ascii encoding of the message. However, the LSB of everything other than the message is junk. Need to find a way to pad the encoding so it encodes the rest as spaces or other filler character.

--- desteg.py
+++ desteg.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as np
+import binascii as bas
 
 # Function will take an image file and return the color bytes of each pixel in a 1D array
 # input: image file name
@@ -22,7 +22,21 @@ def bits_to_bytes(text_bits):
         pointer += 8
     return text_bytes
 
+def save_text(text_bytes, filename):
+    hex_bytes = []
+    for byte in text_bytes:
+        hex_bytes.append(hex(int(byte, 2))[2:].zfill(2))
+
+    output_bytes = []
+    for hex_b in hex_bytes:
+        output_bytes.append(bas.unhexlify(hex_b))
+
+    file = open(filename, 'wb')
+    file.writelines(output_bytes)
+    file.close()
+
 (ibytes, size) = load_image_bytes('test.png')
 tbits = decode_from_bytes(ibytes)
 
 tbytes = bits_to_bytes(tbits)
+save_text(tbytes, 'test.txt')
