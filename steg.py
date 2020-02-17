2366f4d91d19bd0b48655c494e3f9b6a8b8dabfe
Hadi Ahmed <hadiahmed098@gmail.com>
2020-02-16 22:57:22 -0500

    steg.py can now be called from commandline

--- steg.py
+++ steg.py
@@ -1,17 +1,38 @@
 import cv2
 import numpy as np
+import argparse
+import os.path
+
+def main():
+    try:
+        (i_bytes, size) = load_image_bytes(args.input)
+        t_bits = bytes_to_bits(load_text_bytes(args.message, size[0] * size[1] * size[2]))
+
+        o_bytes = encode_bits_in_bytes(i_bytes, t_bits)
+        save_image_bytes(np.asarray(o_bytes), args.output, size)
+    except Exception as e:
+        print('%s: %s' % (type(e).__name__, e))
 
 # Function will take an image file and return the color bytes of each pixel in a 1D array
 # input: image file name
 # output: 1xN numpy array, tuple with image dimensions
 def load_image_bytes(file_name):
+    # Error checking to verify file exists
+    if not os.path.isfile(file_name):
+        raise FileExistsError("input file must exist and be a valid file")
+
     image_bytes = cv2.imread(file_name, cv2.IMREAD_COLOR)
     return image_bytes.flatten(), image_bytes.shape
 
+
 # Function will take a text file and return a string with bytes of the text
 # input: text file name
-# output: byte string of the text file
+# output: byte string of the text file with padding
 def load_text_bytes(file_name, bytes_length):
+    # Error checking to verify file exists
+    if not os.path.isfile(file_name):
+        raise FileExistsError("message file must exist and be a valid file")
+
     with open(file_name, 'rb') as f:
         text_bytes = f.read()
 
@@ -24,6 +45,7 @@ def load_text_bytes(file_name, bytes_length):
 
     return text_bytes
 
+
 # Function will take the text bytes and convert them into bits
 # input: byte string with the text
 # output: bit array
@@ -33,13 +55,14 @@ def bytes_to_bits(text_bytes):
         bits.append(bin(byte)[2:].zfill(8))
     return bits
 
+
 # Function will take bits of a text file and bytes of an image file and use LSB encoding
 # input: image bytes, text bits
 # output: bytes of new image
 def encode_bits_in_bytes(image_bytes, text_bits):
     # Error checking to avoid any out-of-bound errors before encoding
-    if len(ibytes) < len(tbits) * 8:
-        raise IndexError(f"size of image file ({len(ibytes)}) is smaller than size of message ({len(tbits) * 8})")
+    if len(image_bytes) < len(text_bits) * 8:
+        raise IndexError(f"size of image file ({len(image_bytes)}) is smaller than size of message ({len(text_bits) * 8})")
 
     output_bytes = []
     byte_counter = 0
@@ -51,15 +74,25 @@ def encode_bits_in_bytes(image_bytes, text_bits):
     output_bytes = np.concatenate((np.asarray(output_bytes), image_bytes[byte_counter:]))
     return output_bytes
 
+
 # Function will take bytes of an image and convert them into an image
 # input: image bytes in numpy array, output image name, dimensions of the image
 def save_image_bytes(image_bytes, output_file_name, image_dim):
     image_reshape = image_bytes.reshape(image_dim)
     cv2.imwrite(output_file_name, image_reshape)
 
+# Setup commandline parser
+parser = argparse.ArgumentParser(description='Encode an image using steganography', allow_abbrev=True,
+                                 usage='steg.py [-h] <inputfilename> <messagefilename> '
+                                       '[-output <outputfile>]')
 
-(ibytes, size) = load_image_bytes('donut.png')
-tbits = bytes_to_bits(load_text_bytes('message.txt', size[0] * size[1] * size[2]))
+# Add commandline arguments
+parser.add_argument('input', help="image input file, .png")
+parser.add_argument('message', help='message input file, .txt')
+parser.add_argument('-output', default='message_encoded.png', help='image output file, .png')
 
-obytes = encode_bits_in_bytes(ibytes, tbits)
-save_image_bytes(np.asarray(obytes), 'encoded_message.png', size)
+args = parser.parse_args()
+
+# Establish main method
+if __name__ == '__main__':
+    main()
